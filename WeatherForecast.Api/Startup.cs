146e8a5084b90f2d46f97c9b6dd5d9cea9a300cb
Cristiano Motta <cristiano.motta@hotmail.it>
2024-01-18 13:07:18 +0100

    Minor changes

--- WeatherForecast.Api/Startup.cs
+++ WeatherForecast.Api/Startup.cs
@@ -36,7 +36,7 @@ namespace WeatherForecast.Api
             services.AddSingleton(sp => RabbitManager.CreateBus("localhost"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.415
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
